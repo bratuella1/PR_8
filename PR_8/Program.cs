5173630d0f6db1ae9368b748991a588b65832e91
bratuella1 <117741263+bratuella1@users.noreply.github.com>
2022-11-15 04:16:42 +0300

    Add files via upload

---
+++ PR_8/Program.cs
@@ -0,0 +1,50 @@
+﻿//************************
+// практическая работа №8*
+// выполнил: Ханов Артур *
+//************************
+using System;
+
+namespace PR_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, x, c, c1, sum;
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            try
+            {
+                Console.WriteLine("здравствуйте! \nвведите любые числа");
+                i = Convert.ToInt32(Console.ReadLine());
+                x = i;
+                c = 1;
+                c1 = 0;
+                sum = 0;
+
+                do
+                {
+                    c++;
+                    c1 += i;
+                    for (int v = i; v <= i; v++)
+                    {
+                        sum += i * i;
+                    }
+                    if (i < x) x = i;
+                    i = Convert.ToInt32(Console.ReadLine());
+                }
+                while (i != 0);
+                Console.WriteLine($"введено чисел {c}");
+                Console.WriteLine($"общая сумма чисел {c1}");
+                Console.WriteLine($"сумма квадратов {sum}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("что-то пошло не так " + e.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ PR_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PR_8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR_8")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b27713d3-8ce4-4d2c-9c33-33e410d748eb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
